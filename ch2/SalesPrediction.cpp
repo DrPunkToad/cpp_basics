6c154eafc0c4728f0d71f289c2a0662820dbfefa
Emily <84556633+DrPunkToad@users.noreply.github.com>
2025-01-29 03:02:27 -0800

    commit

--- ch2/SalesPrediction.cpp
+++ ch2/SalesPrediction.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     double sales_percent = 0.58;    //58 percent
-    double sales = 8600000;     //$8.6 million
+    double sales = 8600000.0;     //$8.6 million
     double total;   //total east coast division will generate for company
 
     total = sales * sales_percent;
