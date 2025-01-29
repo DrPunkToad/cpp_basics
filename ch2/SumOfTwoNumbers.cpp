7336d5014f0183c79640b686c73bc325bf145780
Emily <84556633+DrPunkToad@users.noreply.github.com>
2025-01-29 02:51:43 -0800

    ch2 commit changes

---
+++ ch2/SumOfTwoNumbers.cpp
@@ -0,0 +1,18 @@
+/*
+Write a program that stores the integers 50 and 100 in variables,
+and stores the sum of these two in a variable named total .
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 50;
+    int b = 100;
+
+    int total = a + b;
+
+    cout << total;
+
+    return 0;
+}
